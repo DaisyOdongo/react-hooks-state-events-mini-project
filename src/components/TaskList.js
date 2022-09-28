cfd79f3b0df1d392d56dfd189fe8dfc10d4fe338
+ <+>
2022-09-28 09:05:29 +0300

    assignment completed

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks, handleDeleteTasks}) {
+  console.log ("My tasks Element", tasks)
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task) =>(
+        <Task 
+          key={tasks.text} 
+          text={tasks.text} 
+          handleDeleteTasks={handleDeleteTasks} 
+          category={tasks.category} 
+        />
+      ))}
     </div>
   );
 }
